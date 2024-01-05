180b7135fc1dfa8f485baac1ef2b13ab2c03e23a
mustafaalani <mustafaalani@192.168.0.233>
2024-01-05 13:04:40 -0700

    Initial Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,13 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Wed");
+		
+		System.out.println("Batch 11");
+		System.out.println("Batch 11 Git Lecture");
+	}
+
+}
